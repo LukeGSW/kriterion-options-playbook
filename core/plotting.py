7a83a640d140fe622621ee63cbac6ca4ba769c84
LukeGSW <lukeguitarskywalker@gmail.com>
2025-08-22 20:15:12 +0200

    Update plotting.py

--- core/plotting.py
+++ core/plotting.py
@@ -1,13 +1,42 @@
 import plotly.graph_objects as go
 import numpy as np
 
-def create_pnl_chart(underlying_range, pnl_at_T, pnl_at_expiration, strategy_name, days_to_expiration):
+def create_pnl_chart(
+    underlying_range, 
+    pnl_at_T, 
+    pnl_at_expiration, 
+    strategy_name, 
+    days_to_expiration,
+    original_pnl_at_T=None,
+    original_pnl_at_expiration=None
+    ):
     """
-    Genera il grafico P/L interattivo con Plotly, mostrando sia la curva a T che a scadenza.
+    Genera il grafico P/L. Se vengono forniti i dati originali,
+    crea un grafico comparativo.
     """
     fig = go.Figure()
 
-    # Linea del P/L a scadenza (solida)
+    # Se ci sono dati originali, plottali prima in grigio trasparente
+    if original_pnl_at_expiration is not None:
+        fig.add_trace(go.Scatter(
+            x=underlying_range, 
+            y=original_pnl_at_expiration, 
+            mode='lines', 
+            name='P/L Originale (Scadenza)',
+            line=dict(color='grey', width=2, dash='dot'),
+            opacity=0.5
+        ))
+    if original_pnl_at_T is not None:
+         fig.add_trace(go.Scatter(
+            x=underlying_range, 
+            y=original_pnl_at_T, 
+            mode='lines', 
+            name='P/L Originale (T)',
+            line=dict(color='grey', width=2, dash='dash'),
+            opacity=0.5
+        ))
+
+    # Linea principale del P/L a scadenza (solida)
     fig.add_trace(go.Scatter(
         x=underlying_range, 
         y=pnl_at_expiration, 
@@ -16,7 +45,7 @@ def create_pnl_chart(underlying_range, pnl_at_T, pnl_at_expiration, strategy_nam
         line=dict(color='royalblue', width=3)
     ))
 
-    # Linea del P/L a T (tratteggiata)
+    # Linea principale del P/L a T (tratteggiata)
     fig.add_trace(go.Scatter(
         x=underlying_range, 
         y=pnl_at_T, 
@@ -28,7 +57,7 @@ def create_pnl_chart(underlying_range, pnl_at_T, pnl_at_expiration, strategy_nam
     # Linea dello zero
     fig.add_hline(y=0, line_width=1, line_dash="dash", line_color="gray")
 
-    # Calcolo punti di breakeven a scadenza
+    # Calcolo punti di breakeven a scadenza (sulla curva principale)
     sign_changes = np.where(np.diff(np.sign(pnl_at_expiration)))[0]
     for be_index in sign_changes:
         fig.add_vline(x=underlying_range[be_index], line_width=1, line_dash="dot", line_color="red",
